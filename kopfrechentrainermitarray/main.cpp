f09f0c2f24414bcd9623246bde4699d56b3fe971
Levin Berberich <Levin.Berberich@BWS.mtk.schule>
2025-02-24 10:04:57 +0100

    kopfrechentrainer updaten

--- kopfrechentrainermitarray/main.cpp
+++ kopfrechentrainermitarray/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int zufallszahl;
+    int zahl1;
+    int zufallszahlArray;
+
+    cout << "Kopfrechntrainer" << endl;
+    for (int i = 0; i < 51; ++i) {
+        zufallszahl = rand();
+    }
+    for (int i = 0; i < 25; ++i) {
+        int zuffalszahlArray;
+        zahl1 = zufallszahlArray[i];
+        cout << zahl1;
+    }
+
+
     return 0;
 }
